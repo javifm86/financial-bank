f4edbe1f97ae60df1aad17f6abbeb7a7ffb18d6c
javifm86 <javifm86@gmail.com>
2021-01-08 16:15:24 +0100

    Add Loading common component

---
+++ src/components/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <div class="grid place-content-center"> -->
+  <div class="loading d-block h-12 w-12 border-gray-900 border-opacity-25 border-4 rounded-full animate-spin"></div>
+  <!-- </div> -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Loading'
+});
+</script>
+
+<style scoped>
+.loading {
+  border-top-color: theme('colors.blue.800');
+}
+</style>
