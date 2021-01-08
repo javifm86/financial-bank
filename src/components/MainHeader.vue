3a1c6c37251341e5ad36b46135eea41e91fc97de
javifm86 <javifm86@gmail.com>
2021-01-08 16:15:15 +0100

    Add MainHeader common component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-white shadow">
+    <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <slot></slot>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainHeader'
+});
+</script>
