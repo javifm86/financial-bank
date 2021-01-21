d0b5f4da78406c1e2c046d1759e529c2c4dbb20f
javifm86 <javifm86@gmail.com>
2021-01-21 13:10:47 +0100

    Remove padding in TableQuotation component

--- src/components/TableQuotation.vue
+++ src/components/TableQuotation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+  <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto">
     <div class="inline-block min-w-full shadow sm:rounded-lg border-b border-gray-200 overflow-hidden">
       <table class="min-w-full divide-y divide-gray-200">
         <thead class="bg-gray-50">
