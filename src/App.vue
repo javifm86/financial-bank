1d76365289557718e6b141501e78b73a4fa22055
javifm86 <javifm86@gmail.com>
2021-02-08 16:06:13 +0100

    Make sure client accounts are loaded always when initializing app
    Accounts are saved in store.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <Menu />
   <main aria-live="polite" class="flex-grow flex-shrink-0">
-    <router-view />
+    <router-view v-if="initialInfoLoaded" />
+    <div v-else class="py-6 grid place-content-center">
+      <Loading />
+    </div>
   </main>
   <Footer />
 </template>
@@ -10,12 +13,36 @@
 import { defineComponent } from 'vue';
 import Menu from '@/layout/Menu.vue'; // @ is an alias to /src
 import Footer from '@/layout/Footer.vue'; // @ is an alias to /src
+import Loading from '@/components/Loading.vue';
+import userService from '@/services/user/user';
+import store from '@/utils/store';
 
 export default defineComponent({
   name: 'App',
   components: {
     Menu,
-    Footer
+    Footer,
+    Loading
+  },
+  data() {
+    return {
+      initialInfoLoaded: false
+    };
+  },
+  created() {
+    userService
+      .getInfo()
+      .then((response) => {
+        const selectedAccount = response.data.accounts[0].id;
+        store.setAccounts(response.data.accounts);
+        store.setSelectedAccount(selectedAccount);
+      })
+      .catch((e) => {
+        console.warn(e);
+      })
+      .finally(() => {
+        this.initialInfoLoaded = true;
+      });
   }
 });
 </script>
