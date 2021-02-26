74714a8377e0e271567a80f8451301650e86919b
javifm86 <javifm86@gmail.com>
2021-02-26 11:54:22 +0100

    Update formatDate

--- src/utils/methods.ts
+++ src/utils/methods.ts
@@ -1,8 +1,21 @@
+type DateTimeStyle = 'full' | 'long' | 'medium' | 'short';
+
+interface DateOptions {
+  dateStyle?: DateTimeStyle;
+  timeStyle?: DateTimeStyle;
+}
+
+interface FormatDateOptions {
+  dateStyle: DateTimeStyle;
+  timeStyle?: DateTimeStyle;
+  hour12: boolean;
+}
+
 export interface Methods {
   formatCurrency(num: number, currency: string, decimals?: number): string;
   formatNumber(num: number, decimals?: number): string;
   formatPercent(num: number, decimals?: number): string;
-  formatDate(str: string): string;
+  formatDate(str: string | number, options?: DateOptions): string;
 }
 
 class GlobalMethods implements Methods {
@@ -30,12 +43,18 @@ class GlobalMethods implements Methods {
       maximumFractionDigits: decimals
     }).format(num / 100);
   }
-  formatDate(str: string) {
+  formatDate(input: string | number, options?: DateOptions) {
+    const dateStyle: DateTimeStyle = options != null && options.dateStyle != null ? options.dateStyle : 'medium';
+    const optionsDate: FormatDateOptions = {
+      dateStyle: dateStyle,
+      hour12: false
+    };
+    if (options != null && options.timeStyle != null) {
+      optionsDate.timeStyle = options.timeStyle;
+    }
     // return new Date(str).toLocaleTimeString('en-US');
     // @ts-ignore
-    return new Intl.DateTimeFormat('en-US', { timeStyle: 'short', dateStyle: 'medium', hour12: false }).format(
-      new Date(str)
-    );
+    return new Intl.DateTimeFormat('en-US', optionsDate).format(new Date(input));
   }
 }
 
