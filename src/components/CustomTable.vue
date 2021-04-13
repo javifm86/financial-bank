fb8c411e0f41cc5db1103233493c6da8de66e305
javifm86 <javifm86@gmail.com>
2021-04-13 13:18:51 +0200

    Add custom table component

---
+++ src/components/CustomTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto">
+    <div class="inline-block min-w-full shadow sm:rounded-lg border-b border-gray-200 overflow-hidden">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+::v-slotted(table) {
+  @apply min-w-full divide-y divide-gray-200;
+}
+::v-slotted(thead) {
+  @apply bg-gray-50;
+}
+::v-slotted(th) {
+  @apply px-5 py-3 text-xs font-semibold text-gray-500 uppercase tracking-wider;
+}
+::v-slotted(tbody) {
+  @apply bg-white divide-y divide-gray-200;
+}
+::v-slotted(td) {
+  @apply p-5 text-sm;
+}
+</style>
