9ab7cd47b2720208cecba411a9634223439b8d92
javifm86 <javifm86@gmail.com>
2021-07-16 12:47:34 +0200

    Update CustomTable component

--- src/components/CustomTable.vue
+++ src/components/CustomTable.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto">
+  <div
+    class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto"
+    :class="[customTdFontSize ? 'custom-td' : 'no-custom-td', customThFontSize ? 'custom-th' : 'no-custom-th']"
+  >
     <div class="inline-block min-w-full shadow sm:rounded-lg border-b border-gray-200 overflow-hidden">
       <slot></slot>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CustomTable',
+  props: {
+    customTdFontSize: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    customThFontSize: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+});
+</script>
 
 <style scoped>
 ::v-slotted(table) {
@@ -13,13 +36,19 @@
 ::v-slotted(thead) {
   @apply bg-gray-50;
 }
+.no-custom-th ::v-slotted(th) {
+  @apply text-xs;
+}
 ::v-slotted(th) {
-  @apply px-5 py-3 text-xs font-semibold text-gray-500 uppercase tracking-wider;
+  @apply px-5 py-3 font-semibold text-gray-500 uppercase tracking-wider;
 }
 ::v-slotted(tbody) {
   @apply bg-white divide-y divide-gray-200;
 }
+.no-custom-td ::v-slotted(td) {
+  @apply text-sm;
+}
 ::v-slotted(td) {
-  @apply p-5 text-sm;
+  @apply p-5;
 }
 </style>
